021da42c6ade30e24358ba7f24fa01134fb2121b
jennaspeer <jenspeer301@hotmail.com>
2025-05-07 14:09:44 +0300

    added js and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Insert Poem",
+    autoStart: true,
+    cursor: "",
+    delay: "natural",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);

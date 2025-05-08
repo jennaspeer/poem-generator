b4111afab09fc788ba3860a7a45e096785ca770d
jennaspeer <jenspeer301@hotmail.com>
2025-05-08 19:19:43 +0300

    added better layout and ai context

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "bef0t2ao2e3269f526fb46599b16a4ab";
   let context =
-    "You are a contemporary poem expert and love to write short poems. Your mission is to generate a 4 line peom in basic HTML without showing the HTML format and seperate each line with a <br/>. Make sure to follow user instructions please.";
+    "You are a contemporary poem expert and love to write short poems. Your mission is to generate a 4 line peom in basic HTML without showing the HTML format and seperate each line with a <br/>. Make sure to follow user instructions please. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
